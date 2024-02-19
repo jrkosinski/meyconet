01984d5f4ad824c638f14703deaee1872384ba40
unknown <john.kosinski@gmail.com>
2024-02-19 10:45:24 +0700

    WSGBaseClassLibrary

---
+++ WSGBaseClassLibrary/WSGFrmBase.cs
@@ -0,0 +1,14 @@
+﻿using WSGUtilitieslib;
+
+namespace WSGBaseClassLibrary
+{
+    public partial class WSGFrmBase : WSGUtilitieslib.Telemetry.Form
+    {
+
+        WSGUtilities wsgUtilities = new WSGUtilities("Meyco Sales Order Tracking");
+        public WSGFrmBase()
+        {
+            InitializeComponent();
+        }
+    }
+}
