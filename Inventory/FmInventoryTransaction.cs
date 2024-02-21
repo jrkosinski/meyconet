52438ab1e7c4e1daf31e82c8ee2f8dcbcea00034
Arvin Roman Agramon <aragramon@gmail.com>
2024-02-21 14:13:46 +0800

    Tab order fixes on Processing, and Maintain menu items

--- Inventory/FmInventoryTransaction.cs
+++ Inventory/FmInventoryTransaction.cs
@@ -5,6 +5,23 @@
         public FrmInventoryTransaction()
         {
             InitializeComponent();
+
+            this.SetTabOrder();
+        }
+
+        protected override void SetTabOrder()
+        {
+            this.SetTabOrder(new Control[] {
+                this.textBoxItem,
+                this.buttonGetItem,
+                this.comboBoxLocation,
+                this.dateTimePickerTdate,
+                this.textBoxQty,
+                this.textBoxCost,
+                this.textBoxNotes,
+                this.buttonSave,
+                this.buttonClose,
+            });
         }
     }
 }
